969e6ee52729381decb2bdaf2a3c083e7c6ed626
aidanperkins2002@gmail.com <aidanperkins2002@gmail.com>
2024-01-30 12:12:45 -0600

    implemented geopy to find the lat/long for the route

--- notamFetch.py
+++ notamFetch.py
@@ -1,25 +1,32 @@
 import requests
 import json
+from geopy.geocoders import Nominatim
 
 outputDecoder = json.decoder.JSONDecoder
+
+geoLocator = Nominatim(user_agent="notam_sort")
 
 creds = {
     "client_id" : "bdffccee1a4141acade40174a7e53bc5",
     "client_secret" : "b5ea7326E4E1461a8e9fc1bcE108aD49",
 }
 
+# get user input here
+depAP = geoLocator.geocode("JFK")
+arrAP = geoLocator.geocode("DFW")
+
 input = {
     "pageSize" : "100",
     "pageNum" : "1",
-    "domesticLocation" : "JFK ",
-    "sortBy" : "featureType",
+    "locationLongitude" : str(depAP.longitude),
+    "locationLatitude" : str(depAP.latitude),
+    "locationRadius" : "25",
 }
 
 
 result = requests.get(url="https://external-api.faa.gov/notamapi/v1/notams", params=input , headers=creds)
 output = result.text
 status = result.status_code
-print(status)
 #print(output)
 
 if status != 200 :
@@ -35,9 +42,7 @@ dataDict = json.load(outputFile)
 outputFile.close()
 
 notamNum = dataDict.get("pageSize")
-print(notamNum)
 pageNum = dataDict.get("pageNum")
-print(pageNum)
 # gets a list of notams the size of the pageSize
 notamData = dataDict.get("items")
 
