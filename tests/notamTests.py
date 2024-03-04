01581e67af50aa74dde39cc8da380ec1d5a4ce75
SirFlarflajon <qcwwindsor@outlook.com>
2024-03-04 17:50:31 -0600

    Add test to check if two airports in the continental US that are far apart return any errors.
    Need to still add tests that are based on longitude/latitude and put in tests that should fail.

--- tests/notamTests.py
+++ tests/notamTests.py
@@ -18,9 +18,14 @@ class TestNotams(unittest.TestCase) :
             notamFetch.get_all_notams("MCI", "OKC")
         except Exception as err:
             self.fail("Airport was not located when it should have been.")
-        pass
 
     # Check two far airports in the continental US
+    
+    def test_far(self) :
+        try:
+            notamFetch.get_all_notams("PWM", "LAX")
+        except Exception as err:
+            self.fail("Airport was not located when it should have been.")
 
     # Check one airport in the continental US and one outside (i.e. Alaska and Hawaii)
 
