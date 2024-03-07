79e1bf7033ff437e70b6361cb08ea0ad78a2b227
James J Williams <112959298+JamesJWilliams3000@users.noreply.github.com>
2024-03-06 18:03:16 -0600

    [KAN-1] Flask now calls notamFetch, displays as a table
    1. After clicking 'search', notamFetch is called
    2. The returned list of notams is displayed as an html table
    2a. I decided to import flask_table to easily create a table from a list of objects. I chose flask_table because it's simple to use for a basic table, and can be easily extended.
    2b. However, I left the NotamTable class inside app.py, I'm not sure if it needs to be moved to it's own file

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, request
 from flask import render_template
 import notamFetch
+from flask_table import Table, Col
+
 app = Flask(__name__)
-#import notamFetch
 
 # Home displays the form for user input
 @app.route("/")
@@ -14,9 +15,45 @@ def home():
 # /query displays the notams
 @app.route('/query/', methods = ['POST', 'GET'])
 def query():
-    if request.method == 'POST':
-        print(notamFetch.get_all_notams(request.form['DepartureAirport'], request.form['ArrivalAirport']), sep='')
+    """Search functionality using the search button is implemented here.
 
+    When a user clicks the 'search' button, notamFetch is called to 
+    retreive all relevant notams, and this list is passed to webpage 
+    /query as a NotamTable.
+    """
+    if request.method == 'POST':
+        # print(*notamFetch.get_all_notams(request.form['DepartureAirport'], request.form['ArrivalAirport']), sep='')
+        departure_airport = request.form['DepartureAirport']
+        arrival_airport = request.form['ArrivalAirport']
+
+        # call backend to retrieve list of notams
+        all_notams = notamFetch.get_all_notams(
+            departure_airport = departure_airport, 
+            arrival_airport = arrival_airport)
+        
         return render_template('query.html', 
-                DepartureAirport = request.form['DepartureAirport'], 
-                ArrivalAirport = request.form['ArrivalAirport'])
+                               table = NotamTable(all_notams, border='1px solid black'),
+                               DepartureAirport = departure_airport, 
+                               ArrivalAirport = arrival_airport)
+    
+class TextCol(Col):
+    """Replaces newlines with <br></br> in a table column for HTML display."""
+
+    def td_format(self, content):
+       return content.replace("\n", "<br />")
+
+class NotamTable(Table):
+    """It's a table...for notams.
+
+    Implements a custom html Table object using flask_table, which will
+    automatically create an html table from a list of Notam objects. 
+    Table can be made like so:
+        table = NotamTable(all_notams)
+    """
+
+    location = Col('Location')
+    number = Col('Number')
+    effective_start = Col('Effective Start')
+    effective_end = Col('Effective End')
+    text = TextCol('Description')
+    type = Col('Type')
