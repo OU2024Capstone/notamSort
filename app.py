e8c393957ad195a08f46a6140f3d2f9669c362b3
potatoh <78054670+aidanperkins@users.noreply.github.com>
2024-03-08 14:59:55 -0600

    [KAN-54] removed that commented out print statement

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ def home():
 @app.route('/query/', methods = ['POST', 'GET'])
 def query():
     if request.method == 'POST':
-        #print(notamFetch.get_all_notams(request.form['DepartureAirport'], request.form['ArrivalAirport']), sep='')
-
         return render_template('query.html', 
                 DepartureAirport = request.form['DepartureAirport'], 
                 ArrivalAirport = request.form['ArrivalAirport'])
