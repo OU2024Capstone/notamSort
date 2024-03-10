a13300262563fbb1e38860704c602e06b6182efe
SirFlarflajon <qcwwindsor@outlook.com>
2024-03-09 20:23:10 -0600

    Add null test Made a test to check if no inputs are given to the front end.

--- tests/notamTests.py
+++ tests/notamTests.py
@@ -18,9 +18,16 @@ class TestNotams(unittest.TestCase) :
         except Exception as err:
             self.fail("Airport was not located when it should have been.")
         
-
     # Test invalid inputs in the frontend
         
+    def test_inputs_invalid_none(self):
+        arrival_airport = None
+        departure_airport = None
+        print("Testing no inputs:")
+        with self.assertRaises(Exception):
+            notamFetch.get_notams_at(departure_airport)
+            notamFetch.get_notams_at(arrival_airport)
+
     def test_inputs_invalid_int(self):
         arrival_airport = 46
         departure_airport = 28
@@ -32,7 +39,7 @@ class TestNotams(unittest.TestCase) :
     def test_inputs_invalid_float(self):
         arrival_airport = 35.3955
         departure_airport = 97.5962
-        print("Testing integer inputs:")
+        print("Testing float decimal inputs:")
         with self.assertRaises(Exception):
             notamFetch.get_notams_at(departure_airport)
             notamFetch.get_notams_at(arrival_airport)
@@ -44,7 +51,7 @@ class TestNotams(unittest.TestCase) :
     def test_close(self) :
         arrival_airport = "MCI" # Kansas City, MO
         departure_airport = "OKC" # Oklahoma City, OK
-        print("Testing flight between " + departure_airport + " and " + arrival_airport)
+        print("Testing flight between " + departure_airport + " and " + arrival_airport + ":")
         try:
             notamFetch.get_all_notams(departure_airport, arrival_airport)
         except Exception as err:
@@ -55,7 +62,7 @@ class TestNotams(unittest.TestCase) :
     def test_far(self) :
         arrival_airport = "PWM" # Portland, ME
         departure_airport = "LAX" # Los Angeles, CA
-        print("Testing flight between " + departure_airport + " and " + arrival_airport)
+        print("Testing flight between " + departure_airport + " and " + arrival_airport + ":")
         try:
             notamFetch.get_all_notams(departure_airport, arrival_airport)
         except Exception as err:
@@ -67,38 +74,36 @@ class TestNotams(unittest.TestCase) :
             
     def test_alaska(self) :
         airport = "JNU" # Juneau, AK
-        print("Testing " + airport)
+        print("Testing " + airport + ":")
         with self.assertRaises(Exception):
             notamFetch.get_notams_at(airport)
 
     def test_hawaii(self) :
         airport = "HNL" # Honolulu, HI
-        print("Testing " + airport)
+        print("Testing " + airport + ":")
         with self.assertRaises(Exception):
             notamFetch.get_notams_at(airport)
 
-    # Check one airport in the continental US and one outside (i.e. Canada, Mexico, etc.)
+    # Check airports in North America (i.e. Canada, Mexico, etc.)
             
     def test_canada(self):
         airport = "YOW" # Ottawa, Canada
-        print("Testing " + airport)
+        print("Testing " + airport + ":")
         with self.assertRaises(Exception):
             notamFetch.get_notams_at(airport)
-
-    # Check two airports outside the continental US 
             
     def test_mexico(self):
         airport = "MEX" # Mexico City, Mexico
-        print("Testing " + airport)
+        print("Testing " + airport + ":")
         with self.assertRaises(Exception):
             notamFetch.get_notams_at(airport)
 
-    # Check two airports in other countries
-    ''''
+    # Check airports outside North America
+    '''' Need to change this to be another country,
     def test_canada_mexico(self):
         arrival_airport = "YOW" # Ottawa, Canada
         departure_airport = "MEX" # Mexico City, Mexico
-        print("Testing " + airport)
+        print("Testing " + airport + ":")
         with self.assertRaises(Exception):
             notamFetch.get_notams_at(airport)
     '''
